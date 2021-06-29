1fab1d6f16c45fe6cf2bbbb7f729494a734863c1
ArturDiasRibeiro <Deathstealer@live.com>
2021-06-29 19:09:55 -0300

    HELP ME JANAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ index.js
@@ -0,0 +1,72 @@
+let listaboss = [
+    {
+        "name": "The Duke of Flies",
+        "id": "67.0",
+        "hp": "110"
+    }
+]
+
+const URL = 'https://projeto-final-ppw.herokuapp.com/api/115609'
+let ul = document.querySelector("#listaboss")
+let form = document.querySelector("#formCadastro")
+let inputName = document.querySelector("#name")
+let inputid = document.querySelector("#id")
+let inputhp = document.querySelector("#hp")
+form.addEventListener("submit", cadastro)
+
+function cadastro(evento){
+    evento.preventDefault()
+    let boss = {
+        "name": inputName.value,
+        "id": inputid.value,
+        "hp": inputhp.value
+    }
+
+    let texto = JSON.stringify(boss)
+
+    const opcoes = {
+        method: "POST",
+        body: texto,
+        headers: {
+            "content-type": "application/json"
+        }
+    }
+    const requisicao = fetch(URL, opcoes)
+    requisicao.then(function(response){
+        console.log(response.statusText)
+        console.log(response.status)
+        getboss()
+    })
+}
+
+function deleteboss(){
+
+}
+
+function printboss(){
+    ul.textContent = ""
+    for(const boss of listaboss){
+        const li = document.createElement("li")
+        li.textContent = `${boss.name} ${boss.hp} (${boss.id})`
+        
+        const button = document.createElement('button')
+        button.textContent = "Deletar"
+        button.onclick = deleteboss
+        
+        li.appendChild(button)
+        ul.appendChild(li)
+    }
+}
+
+function getboss(){
+    const request = fetch(URL)
+    request.then(function(response){
+        const json = response.json()
+        json.then(function(boss){
+            listaboss = boss
+            printboss()
+        })
+    })
+}
+
+getboss()
